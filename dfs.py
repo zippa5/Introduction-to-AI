3147795279e1fd2d2ddf6a73fc498f329deb4567
zippa5 <Trommer.gozie@gmail.com>
2025-07-18 19:24:57 +0200

    add dfs

--- dfs.py
+++ dfs.py
@@ -1,1 +1,31 @@
+graph_unweighted = {
+    'A': ['B', 'C', 'D'],
+    'B': ['E', 'F'],
+    'C': [],
+    'D': ['G'],
+    'E': [],
+    'F': [],
+    'G': []
+}
 
+def dfs(graph, start, goal):
+    visited = set()
+    stack = [[start]]  # Stack of paths
+
+    while stack:
+        path = stack.pop()  # Get last path
+        node = path[-1]
+
+        if node == goal:
+            return path
+
+        if node not in visited:
+            visited.add(node)
+            for neighbor in reversed(graph[node]):
+                new_path = list(path)
+                new_path.append(neighbor)
+                stack.append(new_path)
+
+    return None
+
+print("BFS path:", dfs(graph_unweighted, 'A', 'G'))
--- ucs.py
+++ ucs.py
@@ -1,1 +1,35 @@
+import heapq  # Priority Queue implementation
 
+graph_weighted = {
+    'S': [('A', 1), ('B', 2), ('C', 3)],
+    'A': [('D', 4)],
+    'B': [('E', 2)],
+    'C': [('F', 5)],
+    'D': [('G', 1)],
+    'E': [('G', 1)],
+    'F': [('G', 6)],
+    'G': []
+}
+
+def ucs(graph, start, goal):
+    visited = set()
+    pq = [(0, [start])]  # Priority queue stores (cost, path)
+
+    while pq:
+        cost, path = heapq.heappop(pq)
+        node = path[-1]
+
+        if node == goal:
+            return path, cost
+
+        if node not in visited:
+            visited.add(node)
+            for neighbor, weight in graph[node]:
+                if neighbor not in visited:
+                    new_path = path + [neighbor]
+                    heapq.heappush(pq, (cost + weight, new_path))
+
+    return None, float('inf')
+
+path, cost = ucs(graph_weighted, 'S', 'G')
+print("UCS path:", path, "with cost:", cost)
